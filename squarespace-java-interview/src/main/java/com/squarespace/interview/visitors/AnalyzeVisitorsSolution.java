b1bae794be9dffa97c702ab1d896d0471ae9d501
Bandari, Abinay <abinay.bandari@one.verizon.com>
2018-02-07 16:25:04 -0500

    Fix for vistors

--- squarespace-java-interview/src/main/java/com/squarespace/interview/visitors/AnalyzeVisitorsSolution.java
+++ squarespace-java-interview/src/main/java/com/squarespace/interview/visitors/AnalyzeVisitorsSolution.java
@@ -1,5 +1,10 @@
 package com.squarespace.interview.visitors;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
 class AnalyzeVisitorsSolution {
 
   /**
@@ -12,6 +17,39 @@ class AnalyzeVisitorsSolution {
   @SuppressWarnings("UnusedParameters")
   static long[] processPageViews(Iterable<WebsiteVisit> websiteVisits) {
     // @ToDo Implement this routine.
-    return new long[3];
+    long[] countingVisitors = new long[3];
+    long visitors = 0;
+    long unique_visitors = 0;
+    long sessions = 0;
+    Map<String, Long> allSessions = new HashMap<>();
+    for(WebsiteVisit websiteVisit : websiteVisits){
+      visitors++;
+      if(allSessions.containsKey(websiteVisit.getVisitorId())){
+        if(checkNewSession(allSessions.get(websiteVisit.getVisitorId()),websiteVisit.getTimestamp())){
+          sessions++;
+        }
+      } else {
+        sessions++;
+        unique_visitors++;
+      }
+      allSessions.put(websiteVisit.getVisitorId(), websiteVisit.getTimestamp());
+    }
+    countingVisitors[0] = visitors;
+    countingVisitors[1] = unique_visitors;
+    countingVisitors[2] = sessions;
+    return countingVisitors;
   }
+
+  public static boolean checkNewSession(long existingSessionTime, long newTime){
+    long timeDiffrence =  newTime - existingSessionTime;
+    if(timeDiffrence<0){
+      timeDiffrence = -1*timeDiffrence;
+    }
+    if(timeDiffrence > 30*60){
+      return true;
+    }
+    return false;
+  }
+
+
 }
